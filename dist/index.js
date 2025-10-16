87b291a332fc92e0015972df868163528cac959f
Rohaan <roaan.dev@gmail.com>
2025-10-16 14:58:17 +0500

    3000 -> 3007

--- dist/index.js
+++ dist/index.js
@@ -31,7 +31,7 @@ const profile_1 = require("./routes/profile");
 // Load environment variables
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3007;
 // Middleware
 app.use((0, helmet_1.default)());
 app.use((0, cors_1.default)({
